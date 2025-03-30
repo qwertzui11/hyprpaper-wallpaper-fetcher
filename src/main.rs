2b7bfe95873761e83bf0d7ef5314025dd8d198ae
Markus Lanner <contact@markus-lanner.com>
2025-03-30 20:31:23 +0200

    feature: add `topics` to cli options. fix: used url to set hyprpaper file

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,17 @@ use tracing::{debug, error, info, warn};
 struct CliOptions {
     /// File to use as `api-key`. Must contain a single line.
     #[arg(long)]
-    api_key_file: Option<std::path::PathBuf>,
+    api_key_file: Option<PathBuf>,
     /// `api-key` from https://unsplash.com/developers
     #[arg(long)]
     api_key: Option<String>,
+    /// the topics to search unsplash for. By default `wallpapers` is used. Another example is
+    /// `wallpapers,space`
+    #[arg(long)]
+    topics: Option<String>,
+    /// Were to save the wallpaper to. By default it is `~/Pictures/hyprpaper-wallpaper.jpeg`
+    #[arg(long)]
+    target_path: Option<PathBuf>,
     /// Set the count on how often to retry when the unsplash request fails due to a network issue.
     /// [default: 30]
     #[arg(long)]
@@ -55,8 +62,6 @@ pub enum WallpaperError {
     ReadApiKey(std::io::Error),
     #[error("Unsuccessful request status code")]
     Response(reqwest::StatusCode),
-    #[error("hyprctl failed")]
-    Hyprctl(std::io::Error),
     #[error("hyprpaper failed")]
     Hyprpaper(std::io::Error),
     #[error("could not write image")]
@@ -83,7 +88,7 @@ async fn main() -> WallpaperResult<()> {
     let auth = auth_key(&cli_options)?;
     let retry_count = cli_options.retry_count.unwrap_or(30);
     for _ in 0..retry_count {
-        let download = download_photo(&auth).await;
+        let download = download_photo(&cli_options, &auth).await;
         match download {
             Ok(_) => return Ok(()),
             Err(err) => match err {
@@ -109,10 +114,12 @@ async fn main() -> WallpaperResult<()> {
     Err(WallpaperError::Failed(retry_count))
 }
 
-async fn download_photo(auth: &str) -> WallpaperResult<()> {
+async fn download_photo(cli_options: &CliOptions, auth: &str) -> WallpaperResult<()> {
     // https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html
+    let default_topic = "wallpapers".to_string();
+    let topics = cli_options.topics.as_ref().unwrap_or(&default_topic);
     let request_url =
-        "https://api.unsplash.com/photos/random?topics=wallpapers&orientation=landscape";
+        format!("https://api.unsplash.com/photos/random?topics={topics}&orientation=landscape");
     debug!("getting {}", request_url);
     let mut headers = header::HeaderMap::new();
     headers.insert(
@@ -122,36 +129,38 @@ async fn download_photo(auth: &str) -> WallpaperResult<()> {
     let client = reqwest::Client::builder()
         .default_headers(headers)
         .build()?;
-    let random = do_request(&client, request_url).await?;
+    let random = do_request(&client, &request_url).await?;
     let random = random.json::<RandomResponse>().await?;
     let photo_url = random.urls.full;
     info!(
-        "getting a wallpaper from '{}' with the resolution {}x{}. url: '{}'",
-        random.user.name, random.width, random.height, photo_url
+        "getting the wallpaper (id: {}) with the description '{}' from the author '{}'. It got liked {} times.",
+        random.id,
+        random.description.unwrap_or("no description".to_string()),
+        random.user.name,
+        random.likes
+    );
+    debug!(
+        "Image has the resolution {}x{}. loading url: '{}'",
+        random.width, random.height, photo_url
     );
     let photo = do_request(&client, &photo_url).await?;
     let photo = photo.bytes().await?;
-    let photo_file = photo_file_path()?;
+    let photo_file = photo_file_path(cli_options)?;
     std::fs::write(photo_file.clone(), photo).map_err(WallpaperError::CouldNotWriteImage)?;
     debug!("setting: {:?}", photo_file);
+    let photo_file_str = photo_file.to_str().unwrap();
+    exec_hyprpaper("unload", photo_file_str)?;
+    exec_hyprpaper("preload", photo_file_str)?;
+    exec_hyprpaper("wallpaper", &format!(",{}", photo_file_str))?;
+    Ok(())
+}
+
+fn exec_hyprpaper(command: &str, arg: &str) -> Result<(), WallpaperError> {
+    info!("running `hyprpaper {} {}`", command, arg);
     let result = std::process::Command::new("hyprctl")
         .arg("hyprpaper")
-        .arg("unload")
-        .arg(photo_file.clone())
-        .output()
-        .map_err(WallpaperError::Hyprctl)?;
-    debug!("{:?}", result);
-    let result = std::process::Command::new("hyprctl")
-        .arg("hyprpaper")
-        .arg("preload")
-        .arg(photo_file.clone())
-        .output()
-        .map_err(WallpaperError::Hyprpaper)?;
-    debug!("{:?}", result);
-    let result = std::process::Command::new("hyprctl")
-        .arg("hyprpaper")
-        .arg("wallpaper")
-        .arg(format!(",{}", photo_file.to_str().unwrap()))
+        .arg(command)
+        .arg(arg)
         .output()
         .map_err(WallpaperError::Hyprpaper)?;
     debug!("{:?}", result);
@@ -175,10 +184,13 @@ struct User {
 
 #[derive(Debug, serde::Deserialize)]
 struct RandomResponse {
-    width: i32,
-    height: i32,
+    id: String,
+    description: Option<String>,
     user: User,
     urls: PhotoSrcResponse,
+    width: i32,
+    height: i32,
+    likes: i32,
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -213,9 +225,13 @@ fn auth_key(cli_options: &CliOptions) -> WallpaperResult<String> {
     Ok(format!("Client-ID {}", key))
 }
 
-fn photo_file_path() -> WallpaperResult<PathBuf> {
-    let result = dirs::picture_dir().ok_or(WallpaperError::ConfigDir)?;
-    let result = result.join("hyprpaper-wallpaper.jpeg");
-    let result = std::path::absolute(result).unwrap();
-    Ok(result)
+fn photo_file_path(cli_options: &CliOptions) -> WallpaperResult<PathBuf> {
+    if let Some(path) = &cli_options.target_path {
+        Ok(path.clone())
+    } else {
+        let result = dirs::picture_dir().ok_or(WallpaperError::ConfigDir)?;
+        let result = result.join("hyprpaper-wallpaper.jpeg");
+        let result = std::path::absolute(result).unwrap();
+        Ok(result)
+    }
 }
