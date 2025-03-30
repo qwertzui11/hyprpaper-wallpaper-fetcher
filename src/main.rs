47889b863066ac8727c2fa97d143f03ca0200d5c
Markus Lanner <contact@markus-lanner.com>
2025-03-30 09:57:35 +0200

    feature: add cli options

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+use clap::Parser;
+use humantime::parse_duration;
 use reqwest::header;
 use std::path::PathBuf;
 use thiserror::Error;
 use tokio::time::sleep;
 use tracing::{debug, error, info, warn};
+
+#[derive(Parser)]
+#[command(name = "Hyprpaper Unsplash Wallpaper")]
+#[command(version, about = "Request a wallpaper from https://unsplash.com and sets it to hyprpaper using hyprpaperctl", long_about = None)]
+struct CliOptions {
+    /// File to use as `api-key`. Must contain a single line.
+    #[arg(long)]
+    api_key_file: Option<std::path::PathBuf>,
+    /// `api-key` from https://unsplash.com/developers
+    #[arg(long)]
+    api_key: Option<String>,
+    /// Set the count on how often to retry when the unsplash request fails due to a network issue.
+    /// [default: 30]
+    #[arg(long)]
+    retry_count: Option<u32>,
+    /// Set the retry timeout on how long to wait before retrying to get a new wallpaper.
+    /// [default: 10s]
+    #[arg(long, value_parser = parse_duration)]
+    retry_timeout: Option<std::time::Duration>,
+    /// Set log-severity. Valid values are: `error`, `warn`, `info`, `debug` and `trace`.
+    #[arg(short, long)]
+    verbose: Option<tracing::Level>,
+}
 
 #[derive(Error, Debug)]
 pub enum WallpaperError {
@@ -23,17 +48,19 @@ pub enum WallpaperError {
     #[error("could set auth token")]
     ParseAuthHeader(reqwest::header::InvalidHeaderValue),
     #[error("failed to get a wallpaper after {0} tries")]
-    Failed(i32),
+    Failed(u32),
 }
 
 type WallpaperResult<T> = Result<T, WallpaperError>;
 
 #[tokio::main]
 async fn main() -> WallpaperResult<()> {
+    let cli_options = CliOptions::parse();
+
     tracing_subscriber::fmt::init();
 
-    let auth = auth_key()?;
-    let retry_count = 30;
+    let auth = auth_key(&cli_options)?;
+    let retry_count = cli_options.retry_count.unwrap_or(30);
     for _ in 0..retry_count {
         let download = download_photo(&auth).await;
         match download {
@@ -133,15 +160,19 @@ struct PhotoSrcResponse {
     full: String,
 }
 
-fn api_key() -> WallpaperResult<String> {
+fn default_api_key_file_path() -> WallpaperResult<PathBuf> {
     let config_dir = dirs::config_dir().ok_or(WallpaperError::ConfigDir)?;
-    let api_file = config_dir.join("unsplash-key");
+    Ok(config_dir.join("unsplash-key"))
+}
+
+fn api_key() -> WallpaperResult<String> {
+    let api_file = default_api_key_file_path()?;
     let api_key = std::fs::read_to_string(api_file).map_err(WallpaperError::ReadApiKey)?;
     let api_key = api_key.trim().to_owned();
     Ok(api_key)
 }
 
-fn auth_key() -> WallpaperResult<String> {
+fn auth_key(cli_options: &CliOptions) -> WallpaperResult<String> {
     api_key().map(|k| format!("Client-ID {}", k))
 }
 
